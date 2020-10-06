d469380aff03f120c050731e0f7768ec63990660
James Belta <james.belta@gmail.com>
2020-10-06 15:51:29 -0600

    Build shelter  show test

---
+++ spec/features/shelter/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor, when I visit /shelters/:id" do
+  before :each do
+    @shelter_1 = Shelter.create(name: "Puppy Palace", address: "123 Lane", city: "Wepo", state: "CT", zip: 20302)
+    @shelter_2 = Shelter.create(name: "Cat Cabin", address: "456 Road", city: "Tops", state: "NC", zip: 12321)
+  end
+  it "Shows the shelters address, city, state and zip" do
+    visit "/shelters/#{@shelter_1.id}"
+
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_1.address)
+    expect(page).to have_content(@shelter_1.city)
+    expect(page).to have_content(@shelter_1.state)
+    expect(page).to have_content(@shelter_1.zip)
+
+    visit "/shelters/#{@shelter_2.id}"
+    
+    expect(page).to have_content(@shelter_2.name)
+    expect(page).to have_content(@shelter_2.address)
+    expect(page).to have_content(@shelter_2.city)
+    expect(page).to have_content(@shelter_2.state)
+    expect(page).to have_content(@shelter_2.zip)
+  end
+end
