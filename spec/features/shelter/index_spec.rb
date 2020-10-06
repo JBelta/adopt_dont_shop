12102f86a48ddb65250f4190bd64a718b1d81e5d
James Belta <james.belta@gmail.com>
2020-10-06 14:04:31 -0600

    Added instance shelter variables before seeds

--- spec/features/shelter/index_spec.rb
+++ spec/features/shelter/index_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor, when I visit /shelters" do
+  before :each do
+    @shelter_1 = Shelter.create(name: "Puppy Palace")
+    @shelter_2 = Shelter.create(name: "Cat Cabin")
+  end
   it "Has a name of each shelter in the system" do
     visit "/shelters"
 
