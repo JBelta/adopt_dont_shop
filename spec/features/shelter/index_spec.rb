7432e8e2820d1a8e6dedc9bf524f4d28b655a56e
James Belta <james.belta@gmail.com>
2020-10-06 15:46:49 -0600

    Added additional values to shelters

--- spec/features/shelter/index_spec.rb
+++ spec/features/shelter/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor, when I visit /shelters" do
   before :each do
-    @shelter_1 = Shelter.create(name: "Puppy Palace")
-    @shelter_2 = Shelter.create(name: "Cat Cabin")
+    @shelter_1 = Shelter.create(name: "Puppy Palace", address: "123 Lane", city: "Wepo", state: "CT", zip: 20302)
+    @shelter_2 = Shelter.create(name: "Cat Cabin", address: "456 Road", city: "Tops", state: "NC", zip: 12321)
   end
   it "Has a name of each shelter in the system" do
     visit "/shelters"
@@ -11,4 +11,6 @@ RSpec.describe "As a visitor, when I visit /shelters" do
     expect(page).to have_content(@shelter_1.name)
     expect(page).to have_content(@shelter_2.name)
   end
+
+  it ""
 end
