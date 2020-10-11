064d57dd174157cbaa761ec3ba54c498d1b71da1
James Belta <james.belta@gmail.com>
2020-10-11 15:17:01 -0600

    Update spec so pets will have :shelter_id

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "As a visitor, when I visit /pets" do
   before :each do
     @shelter_1 = Shelter.create(name: "Puppy Palace", address: "123 Lane", city: "Wepo", state: "CT", zip: 20302)
-    @pet_1 = Pet.create(image: 'dog_01.jpg', name: "Maxine", approximate_age: "7", sex: "F", name_of_shelter: @shelter_1.name, description: "Sweet and Loyal")
+    @pet_1 = @shelter_1.pets.create(image: 'dog_01.jpg', name: "Maxine", approximate_age: "7", sex: "F", name_of_shelter: @shelter_1.name, description: "Sweet and Loyal")
   end
-  it "Has a name of each pet in the system" do
+  it "Has each pet with their information" do
     visit "/pets"
+
     expect(page).to have_content(@pet_1.name)
     # "img" css selector/ class or image wont render
     expect(page).to have_xpath("//img['#{@pet_1.image}']")
@@ -14,26 +15,4 @@ RSpec.describe "As a visitor, when I visit /pets" do
     expect(page).to have_content(@pet_1.sex)
     expect(page).to have_content(@pet_1.name_of_shelter)
   end
-
-  #it "Has a link to create a new shelter" do
-  #  visit "/shelters"
-  #  expect(page).to have_link("New Shelter")
-
-  #  click_on("New Shelter")
-  #  expect(current_path).to eq("/shelters/new")
-  #end
-
-  #it "Redirects to /shelters and has the new #shelter" do
-  #  visit "/shelters/new"
-
-  #  fill_in :name, with: 'Animal House'
-  #  fill_in :address, with: '789 New Drive'
-  #  fill_in :city, with: 'SouthPo'
-  #  fill_in :state, with: 'CT'
-  #  fill_in :zip, with: 55555
-
-  #  click_on 'Submit'
-  #  expect(current_path).to eq('/shelters')
-  #  expect(page).to have_content('Animal #House')
-  #end
 end
