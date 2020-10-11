b5825c3f15209d7890ea5a78870616b23093fd2d
James Belta <james.belta@gmail.com>
2020-10-11 16:23:08 -0600

    edit seed @pet_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,5 @@
 
 #pets
 @pet_1 = @shelter_1.pets.create(image: 'dog_01.jpg', name: "Maxine", approximate_age: "7", sex: "F", name_of_shelter: @shelter_1.name, description: "Sweet and Loyal")
-@pet_2 = @shelter_2.pets.create(image: 'dog_02.jpg', name: "Slobber", approximate_age: "2", sex: "M", name_of_shelter: @shelter.name, description: "Protective and Active")
+
+@pet_2 = Pet.create(image: 'dog_02.jpg', name: "Slobber", approximate_age: "2", sex: "M", name_of_shelter: @shelter.name, description: "Protective and Active", shelter_id: @shelter_1.id)
